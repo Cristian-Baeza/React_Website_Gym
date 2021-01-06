6cc3e0ce2895dc3cdec1a9f7f972b5347eee8f07
Cristian-Baeza <cristianbaeza092@gmail.com>
2021-01-05 19:23:35 -0700

    edited store link

--- src/components/StoreComponent.js
+++ src/components/StoreComponent.js
@@ -6,17 +6,15 @@ export default function Store() {
 
       <div className="shake">
         <div className="target ptext">
-          <p className="font-weight-bold border">
-         <a href="https://rollyfightwear.com/store/10thplanet/" target="_blank" rel="noopener noreferrer">CLICK HERE FOR STORE!</a>
-        </p>
+
           <img src="assets/images/amir_yell.png" alt='amir yelling' className="pimg1" />
         </div>
       </div>
       <section className="section section-light">
         <h2 className="font-weight-bold">
-        MORE COMING SOON!
-        
-      </h2>
+          <a href="https://rollyfightwear.com/store/10thplanet/" target="_blank" rel="noopener noreferrer">CLICK HERE FOR STORE!</a>
+
+        </h2>
       </section>
     </>
   )
